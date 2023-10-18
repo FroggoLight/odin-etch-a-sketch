632f7902fd6882415d22100ec3459a91b46fe10e
FroggoLight <ineedsomefriendsss@gmail.com>
2023-10-18 14:27:11 -0400

    created function to add 16x16 grid

--- functions.js
+++ functions.js
@@ -1,7 +1,22 @@
 const mainMiddle = document.getElementById("middleMain");
 
-const para = document.createElement("div");
-para.className = "small-boxes"
+//let para = document.createElement("div");
+//para.className = "small-boxes"
 
 
-mainMiddle.appendChild(para);
+//mainMiddle.appendChild(para);
+
+function doStuff() {
+    for (i = 0; i < 16; i++) {
+        let column = document.createElement("div");
+        column.className = "sketch-column";
+        
+        for (j = 0; j < 16; j++) {
+            let row = document.createElement("div");
+            row.className = "sketch-row";
+            column.appendChild(row);
+        }
+        
+        mainMiddle.appendChild(column);
+    }
+}
